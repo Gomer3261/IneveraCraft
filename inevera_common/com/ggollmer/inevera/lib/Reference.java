620a533912629308fb78d94b3a293b035abe2697
Geoff Gollmer <Geoff.Gollmer@Gmail.com>
2013-09-10 15:01:53 -0700

    Commit for version Pre1.

--- inevera_common/com/ggollmer/inevera/lib/Reference.java
+++ inevera_common/com/ggollmer/inevera/lib/Reference.java
@@ -15,9 +15,9 @@ public class Reference
 	
 	public static final String MOD_ID = "inevera";
 	public static final String MOD_NAME = "IneveraCraft";
-	public static final String MOD_VERSION = "@VERSION@";
+	public static final String MOD_VERSION = "@VERSION@ (build @BUILD_NUMBER@)";
 	public static final String CHANNEL_NAME = MOD_ID;
-    public static final String DEPENDENCIES = "required-after:Forge@[7.7.1.650,)";
+    public static final String DEPENDENCIES = "required-after:Forge@[9.10.0.804,)";
     public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final int SECOND_IN_TICKS = 20;
     public static final int SHIFTED_ID_RANGE_CORRECTION = 256;
